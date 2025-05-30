3d6e1bead503e97c42c1880ce0352351dfc2a94e
umeshkedimi <umesh836@gmail.com>
2025-05-30 13:43:07 +0530

    Updated path for parent directory

--- mmm_ui/settings/base.py
+++ mmm_ui/settings/base.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dummy-secret")
 
